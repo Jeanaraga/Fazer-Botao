60e2512e9706f1556e784113e7cd4c94c8bb2340
Jean Pedro <jeanjesuspedrobook@gmail.com>
2023-12-21 14:26:27 -0300

    verificações add

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    let widthValor = parseInt(document.getElementById('largura').value);
-    let heightValor = parseInt(document.getElementById('altura').value);
-    let bdRadius = parseInt(document.getElementById('borderRadius').value);
-    let radioValor = document.querySelector('input[name="simOuNao"]:checked').value;
-    let sombraValor = document.querySelector('input[name="snSombra"]:checked').value;
-    let corFundoValor = document.getElementById('corFundo').value;
-    let corTextoValor = document.getElementById('corTexto').value;
+  let widthValor = parseInt(document.getElementById("largura").value);
+  let heightValor = parseInt(document.getElementById("altura").value);
+  let bdRadius = parseInt(document.getElementById("borderRadius").value);
+  let radioValor = document.querySelector(
+    'input[name="simOuNao"]:checked'
+  ).value;
+  let sombraValor = document.querySelector(
+    'input[name="snSombra"]:checked'
+  ).value;
+  let corFundoValor = document.getElementById("corFundo").value;
+  let corTextoValor = document.getElementById("corTexto").value;
 
-    let resultadoBotão = document.getElementById('resulte');
-    resultadoBotão.classList.add('ativar');
+  
+  if (isNaN(widthValor) || isNaN(heightValor) || isNaN(bdRadius)) {
+    alert(
+      "Por favor, insira valores numéricos válidos para largura, altura e raio de borda."
+    );
+    return;
+  }
 
-    resultadoBotão.style.width = widthValor + 'px';
-    resultadoBotão.style.height = heightValor + 'px';
-    resultadoBotão.style.borderRadius = bdRadius + 'px';
-    resultadoBotão.style.backgroundColor = corFundoValor;
-    resultadoBotão.style.color = corTextoValor;
+  if (widthValor < 0 || heightValor < 0 || bdRadius < 0) {
+    alert(
+      "Os valores de largura, altura e raio de borda devem ser não negativos."
+    );
+    return;
+  }
 
-    if (radioValor == 'sim') {
-        resultadoBotão.style.border = '1.5px solid #000';
-    } else if (radioValor == 'nao') {
-        resultadoBotão.style.border = 'none';
-    }
+  let resultadoBotão = document.getElementById("resulte");
+  resultadoBotão.classList.add("ativar");
 
-    if (sombraValor == 'sim') {
-        resultadoBotão.style.boxShadow = '10px 10px 10px rgba(0, 0, 0, 0.3)';
-    }
+  resultadoBotão.style.width = widthValor + "px";
+  resultadoBotão.style.height = heightValor + "px";
+  resultadoBotão.style.borderRadius = bdRadius + "px";
+  resultadoBotão.style.backgroundColor = corFundoValor;
+  resultadoBotão.style.color = corTextoValor;
+
+  if (radioValor == "sim") {
+    resultadoBotão.style.border = "1.5px solid #000";
+  } else if (radioValor == "nao") {
+    resultadoBotão.style.border = "none";
+  }
+
+  if (sombraValor == "sim") {
+    resultadoBotão.style.boxShadow = "10px 10px 10px rgba(0, 0, 0, 0.3)";
+  }
 });
